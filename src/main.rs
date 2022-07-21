b438ad142a680ed1c8154bc2e5421c989132fa28
panekj <me@panekj.dev>
2022-07-21 16:37:07 +0200

    feat: add option to enable/disable plugin

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ pub struct PluginInfo {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Configuration {
     system_lsp: bool,
+    enabled: bool,
     options: Option<Value>,
 }
 
@@ -24,21 +25,23 @@ impl LapcePlugin for State {
     fn initialize(&mut self, info: serde_json::Value) {
         eprintln!("starting plugin");
         let info = serde_json::from_value::<PluginInfo>(info).unwrap();
+        
+        if info.configuration.enabled {
+            let exec_path = if info.configuration.system_lsp {
+                "julia".to_string()
+            } else {
+                // TODO: implement LSP installation
+                "".to_string()
+            };
 
-        let exec_path = if info.configuration.system_lsp {
-            "julia".to_string()
-        } else {
-            // TODO: implement LSP installation
-            "".to_string()
-        };
+            eprintln!("exec path: {}", &exec_path);
 
-        eprintln!("exec path: {}", &exec_path);
-
-        start_lsp(
-            &exec_path,
-            "julia",
-            info.configuration.options,
-            info.configuration.system_lsp,
-        );
+            start_lsp(
+                &exec_path,
+                "julia",
+                info.configuration.options,
+                info.configuration.system_lsp,
+            );
+        }
     }
 }
